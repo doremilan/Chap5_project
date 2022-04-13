891f3926e65a251ea1dd5bada81432acc260f643
chasungbin <lucky9397@naver.com>
2022-04-13 18:52:24 +0900

    ee

--- app.js
+++ app.js
@@ -33,6 +33,7 @@ db.on("error", console.error.bind(console, "connection error:"));
 
 app.use(cors({ origin: "http://localhost:3000" }));
 
+
 app.get("/cors-test", (req, res) => {
   res.send("hi");
 });
@@ -56,12 +57,21 @@ io.on("connection", function (socket) {
   });
 });
 
+
 const commentsRouter = require("./routes/comment");
 const usersRouter = require("./routes/user");
 const postsRouter = require("./routes/post");
 
+//미들웨어 동작시 동작 시간 
+const requestMiddleware = (req, res, next) => {
+  console.log("Request URL:", req.originalUrl, " - ", new Date());
+  next();
+};
+
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+//미들웨어 요청시각표시
+app.use(requestMiddleware);
 app.use("/api", [commentsRouter, usersRouter, postsRouter]);
 
 app.listen(port, () => {
